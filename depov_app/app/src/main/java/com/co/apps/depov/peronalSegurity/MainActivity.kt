b45545d7c1b6c49a2b31af89f5458dc0a7129867
Santiago <144735593+TsantiG@users.noreply.github.com>
2025-02-09 14:49:21 -0500

    update xml parcial
    vistas del proyecto _comienzo_1_

---
+++ depov_app/app/src/main/java/com/co/apps/depov/peronalSegurity/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.co.apps.depov.peronalSegurity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnregister: Button
+    private lateinit var btnlogin: Button
+    private lateinit var btnfacebook: ImageView
+    private lateinit var btntwitter: ImageView
+    private lateinit var btninstagram: ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btninit()
+        btnhome()
+        btnredes()
+
+    }
+
+    fun btninit(){
+        btnregister = findViewById(R.id.btn_register_home)
+        btnlogin = findViewById(R.id.btn_login_home)
+        btnfacebook = findViewById(R.id.img_facebook_home)
+        btntwitter = findViewById(R.id.img_twitter_home)
+        btninstagram = findViewById(R.id.img_instagram_home)
+    }
+
+    fun btnhome(){
+        btnregister.setOnClickListener {
+            val intent = Intent(this, MainRegister::class.java)
+            startActivity(intent)
+        }
+
+        btnlogin.setOnClickListener {
+            val intent = Intent(this, MainLogin::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun btnredes(){
+        btnfacebook.setOnClickListener {
+            val url = "https://www.facebook.com/depov.com"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
+        btntwitter.setOnClickListener {
+            val url = "https://twitter.com/depov_com"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
+        btninstagram.setOnClickListener {
+            val url = "https://www.instagram.com/depov.com"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+    }
+
+
+}
